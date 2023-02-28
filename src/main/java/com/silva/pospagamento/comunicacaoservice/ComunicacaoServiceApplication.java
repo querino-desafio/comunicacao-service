21847c5f48ecdb6f69c7b380a257b3f23ac2ebd7
Dorival Querino da Silva <dorival.querino@gmail.com>
2023-02-27 23:10:27 -0300

    first commit

---
+++ src/main/java/com/silva/pospagamento/comunicacaoservice/ComunicacaoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.silva.pospagamento.comunicacaoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicacaoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComunicacaoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silva/pospagamento/comunicacaoservice/ComunicacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silva.pospagamento.comunicacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
